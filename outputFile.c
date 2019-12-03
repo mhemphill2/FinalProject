cb8178fdd71ea0a96ce2c9eee5b49426ca581c92
nsholm_win <56319291+nikolaj-h@users.noreply.github.com>
2019-12-02 18:34:48 -0600

    outputfile.c with one and two arrays

--- outputFile.c
+++ outputFile.c
@@ -1,9 +1,34 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int outputFile(double* collisions) {
 
-	char [] = "collision_detection.out "
+//sugested input arguments is an array storing the collisions 
+//ID's and the length of this array
+int outputFile() {
+	char filename[] = "collision_detection.out ";
 
-	FILE* fp = fpopen	
+	FILE* fp = fopen(filename, "w");
+
+	fprintf(fp, "s,t\n");
+
+	int n = 8;
+	int meshid[] = { 1,3,4,5,7,33,53,123 };
+	int sphereid[] = { 2,3,55,67,83,123,132,155 };
+	int collisions[] = { 1,3,4,5,7,33,53,123,2,3,55,67,83,123,132,155 };
+
+
+	int c = 0;
+	for (size_t i = 0; i < n; i++)
+	{
+		//solution for 2 arrays storing ID's
+		fprintf(fp, "%d,%d\n", meshid[i], sphereid[i]);
+		//alternative with one array storing ID's in rowmajor. 
+		//Don't know which one is faster though.
+		printf("%d,%d\n", collisions[c],collisions[c+1]);
+		c=c+2;
+	}
+
+	fclose(fp);
+
+	
 }
