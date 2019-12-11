96fa6812a7c7bfb593cb1db3ec3fa6ef0dcc1b07
mike lin <mhemphill2@wisc.edu>
2019-12-11 15:31:06 -0800

    detect closest points on line and plane

--- collisionDetection.c
+++ collisionDetection.c
@@ -12,8 +12,12 @@ int main() {
 
 
 	//What is the exact filenames/types and how should we read them? input arguments?
-	const char filename[] = "mesh.input.obj";
-	const char filename1[] = "spheres.input.csv";
+//	const char filename[] = "mesh.input.obj";
+//	const char filename1[] = "spheres.input.csv";
+//	const char filename[] = "box.obj";
+//	const char filename1[] = "test_vercities.csv";
+	const char filename[] = "2tri_test.obj";
+	const char filename1[] = "1testsphere.csv";
 
 	//Initialize mesh metrics
 	int nTriangles = 0;
@@ -74,6 +78,12 @@ int main() {
 	double ax, ay, az, bx, by, bz, nxx, nyy, nzz, nx, ny, nz;	// normal vectors
 	double pox, poy, poz, D, distance, distancecheck;	// distance and point on plane
 //	double AB[3], BC[3], CA[3], PA[3], PB[3], PC[3];	// cross products to determine in point inside triangle
+
+
+	float dotAC, dotACAB, dotACAP, dotAB, dotABAP, dottest, u, v;
+	double test[3] = {1,2,3};
+	double test1[3] = {2,3,4};
+
 	for (sph = 0; sph < ns; sph++) {
 		for (tri = 0; tri < nTriangles; tri++) {
 			printf("\nSphere ID: %d  ---  Triangle ID: %d\n", sph, tri);
@@ -133,27 +143,101 @@ int main() {
 
 		// to determine if point is inside triangle -- building vectors
 		// may not be necessary...
-			double AB[3] = {mesh[tri * 9 + 3] - mesh[tri * 9 + 0], mesh[tri * 9 + 4] - mesh[tri * 9 + 1], mesh[tri * 9 + 5] - mesh[tri * 9 + 2]};
-			double BC[3] = {mesh[tri * 9 + 6] - mesh[tri * 9 + 3], mesh[tri * 9 + 7] - mesh[tri * 9 + 4], mesh[tri * 9 + 8] - mesh[tri * 9 + 5]};
-			double CA[3] = {mesh[tri * 9 + 0] - mesh[tri * 9 + 6], mesh[tri * 9 + 1] - mesh[tri * 9 + 7], mesh[tri * 9 + 2] - mesh[tri * 9 + 8]};
 
-			double AP[3] = {pox - mesh[tri * 9 + 0], poy - mesh[tri * 9 + 1], poz - mesh[tri * 9 + 2]};
-			double BP[3] = {pox - mesh[tri * 9 + 3], poy - mesh[tri * 9 + 4], poz - mesh[tri * 9 + 5]};
-			double CP[3] = {pox - mesh[tri * 9 + 6], poy - mesh[tri * 9 + 7], poz - mesh[tri * 9 + 8]};
+
+//			double AP[3] = {pox - mesh[tri * 9 + 0], poy - mesh[tri * 9 + 1], poz - mesh[tri * 9 + 2]};
+//			double BP[3] = {pox - mesh[tri * 9 + 3], poy - mesh[tri * 9 + 4], poz - mesh[tri * 9 + 5]};
+//			double CP[3] = {pox - mesh[tri * 9 + 6], poy - mesh[tri * 9 + 7], poz - mesh[tri * 9 + 8]};
 			
 
 			// nx = ay*bz - az*by  			;  ny = az*bx - ax*bz  		;  nz = ax*by - ay*bx
 			// cross products
-			double ABAP[3] = {(AB[1]*AP[2]) - (AB[2]*AP[1]), (AB[2]*AP[0]) - (AB[0]*AP[2]), (AB[0]*AP[1]) - (AB[1]*AP[0])};
-			double BCBP[3] = {(BC[1]*BP[2]) - (BC[2]*BP[1]), (BC[2]*BP[0]) - (BC[0]*BP[2]), (BC[0]*BP[1]) - (BC[1]*BP[0])};
-			double CACP[3] = {(CA[1]*CP[2]) - (CA[2]*CP[1]), (CA[2]*CP[0]) - (CA[0]*CP[2]), (CA[0]*CP[1]) - (CA[1]*CP[0])};
+//			double ABAP[3] = {(AB[1]*AP[2]) - (AB[2]*AP[1]), (AB[2]*AP[0]) - (AB[0]*AP[2]), (AB[0]*AP[1]) - (AB[1]*AP[0])};
+//			double BCBP[3] = {(BC[1]*BP[2]) - (BC[2]*BP[1]), (BC[2]*BP[0]) - (BC[0]*BP[2]), (BC[0]*BP[1]) - (BC[1]*BP[0])};
+//			double CACP[3] = {(CA[1]*CP[2]) - (CA[2]*CP[1]), (CA[2]*CP[0]) - (CA[0]*CP[2]), (CA[0]*CP[1]) - (CA[1]*CP[0])};
 
 
-			printf("ABAP 1,2,3 = %f , %f , %f   \n", ABAP[0], ABAP[1], ABAP[2]);
-			printf("BCBP 1,2,3 = %f , %f , %f   \n", BCBP[0], BCBP[1], BCBP[2]);
-			printf("CACP 1,2,3 = %f , %f , %f   \n", CACP[0], CACP[1], CACP[2]);
+//			printf("ABAP 1,2,3 = %f , %f , %f   \n", ABAP[0], ABAP[1], ABAP[2]);
+//			printf("BCBP 1,2,3 = %f , %f , %f   \n", BCBP[0], BCBP[1], BCBP[2]);
+//			printf("CACP 1,2,3 = %f , %f , %f   \n", CACP[0], CACP[1], CACP[2]);
+
+		// testing if point inside of triangle
+
+		
+			double AB[3] = {mesh[tri * 9 + 3] - mesh[tri * 9 + 0], mesh[tri * 9 + 4] - mesh[tri * 9 + 1], mesh[tri * 9 + 5] - mesh[tri * 9 + 2]};
+			double AC[3] = {mesh[tri * 9 + 6] - mesh[tri * 9 + 0], mesh[tri * 9 + 7] - mesh[tri * 9 + 1], mesh[tri * 9 + 8] - mesh[tri * 9 + 2]};
+			double AP[3] = {pox - mesh[tri * 9 + 0], poy - mesh[tri * 9 + 1], poz - mesh[tri * 9 + 2]};
+
+			//calc dot products
+			dotAC = 0;
+			dotACAB = 0;
+			dotACAP = 0;
+			dotAB = 0;
+			dotABAP = 0;
+		
+			for (int dot = 0; dot < 3; dot++) {
+				dotAC += AC[dot]*AC[dot];
+				dotACAB += AC[dot]*AB[dot];
+				dotACAP += AC[dot]*AP[dot];
+				dotAB += AB[dot]*AB[dot];
+				dotABAP += AB[dot]*AP[dot];
+			}	
+
+			u = ((dotAB * dotACAP) - (dotACAB * dotABAP)) / ((dotAC *dotAB)- (dotACAB*dotACAB));
+			v = ((dotAC * dotABAP) - (dotACAB * dotACAP)) / ((dotAC *dotAB)- (dotACAB*dotACAB));
+			printf("u = %f  v = %f\n", u,v);
 			
+
+			if (u >= 0 && v >= 0 && u+v <= 1) {
+				printf("Projected point is on or in triangle (closest P)\n");
+				// this is closed point, compare to radius
+				// compare po to P with radius
+				// return closest point
+				if ( distance <= rad) {
+					printf("SHPHERE TRIANGE INTERSECT: %d, %d\n",sph, tri);
+				}
+
+			}
+				
+			else {
+				printf("Projected point is NOT on or in triangle\n");
+			}
+			
+			// testing all point to all 3 lines
+			
+			double ab[3] = {mesh[tri * 9 + 3] - mesh[tri * 9 + 0], mesh[tri * 9 + 4] - mesh[tri * 9 + 1], mesh[tri * 9 + 5] - mesh[tri * 9 + 2]};
+			double bc[3] = {mesh[tri * 9 + 6] - mesh[tri * 9 + 3], mesh[tri * 9 + 7] - mesh[tri * 9 + 4], mesh[tri * 9 + 8] - mesh[tri * 9 + 5]};
+			double ca[3] = {mesh[tri * 9 + 0] - mesh[tri * 9 + 6], mesh[tri * 9 + 1] - mesh[tri * 9 + 7], mesh[tri * 9 + 2] - mesh[tri * 9 + 8]};
+			// use po or p????			
+			double ap[3] = {pox - mesh[tri * 9 + 0], poy - mesh[tri * 9 + 1], poz - mesh[tri * 9 + 2]};
+			double bp[3] = {pox - mesh[tri * 9 + 3], poy - mesh[tri * 9 + 4], poz - mesh[tri * 9 + 5]};
+			double cp[3] = {pox - mesh[tri * 9 + 6], poy - mesh[tri * 9 + 7], poz - mesh[tri * 9 + 8]};
+
+			// need to loop through 3 lines
+			double t0 = (ab[0]*ap[0])+(ab[1]*ap[1])+(ab[2]*ap[2]);
+			double t1 = (bc[0]*bp[0])+(bc[1]*bp[1])+(bc[2]*bp[2]);
+			double t2 = (ca[0]*cp[0])+(ca[1]*cp[1])+(ca[2]*cp[2]);
+			printf("t0 , t1 , t2  =  %f , %f , %f \n", t0, t1, t2);
+			if ((t0 <= 0 || t0 >= 1) && (t1 <= 0 || t1 >= 1) && (t2 <= 0 || t2 >= 1)){
+				printf("Projected points are not on lines, go to vertices\n");
+			}
+
+			else {
+
+				t0 = t0 / (ab[0]*ab[0])+(ab[1]*ab[1])+(ab[2]*ab[2]);
+				t1 = t1 / (bc[0]*bc[0])+(bc[1]*bc[1])+(bc[2]*bc[2]);
+				t2 = t2 / (ca[0]*ca[0])+(ca[1]*ca[1])+(ca[2]*ca[2]);
+			double  pline0[3] = {mesh[tri * 9 + 0] + (t0*ab[0]), mesh[tri * 9 + 1] + (t0*ab[1]), mesh[tri * 9 + 2] + (t0*ab[2])};
+			double  pline1[3] = {mesh[tri * 9 + 3] + (t1*bc[0]), mesh[tri * 9 + 4] + (t1*bc[1]), mesh[tri * 9 + 5] + (t1*bc[2])};
+			double  pline2[3] = {mesh[tri * 9 + 6] + (t2*ca[0]), mesh[tri * 9 + 7] + (t2*ca[1]), mesh[tri * 9 + 8] + (t2*ca[2])};
+				printf("Projected point on line is closest\n");
+				printf("New closest point on line : (pline0 = %f ,  %f ,  %f) \n", pline0[0], pline0[1], pline0[2]);
+				printf("New closest point on line : (pline1 = %f ,  %f ,  %f) \n", pline1[0], pline1[1], pline1[2]);
+				printf("New closest point on line : (pline2 = %f ,  %f ,  %f) \n", pline2[0], pline2[1], pline2[2]);
+			}
+
 		}
+
 	}
 
 
