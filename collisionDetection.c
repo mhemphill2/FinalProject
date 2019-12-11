9b1a6460fac6692d2d1fa82da813ac08ab93f0b5
mike lin <mhemphill2@wisc.edu>
2019-12-10 17:09:41 -0800

    cleaned up code and added point projection onto plane

--- collisionDetection.c
+++ collisionDetection.c
@@ -10,16 +10,12 @@ int main() {
 	//What is the exact filenames/types and how should we read them? input arguments?
 	const char filename[] = "mesh.input.obj";
 	const char filename1[] = "spheres.input.csv";
-	//Why is the step beneath done??
-	int* readInputMeshEntries(filename);
-	double* readInputMesh(filename);
-	double* InputSpheresradNS(filename1);
-	double* InputSpheresCoord(filename1);
 
 	//Initialize mesh metrics
 	int nTriangles = 0;
 	int nMeshEntries = 0;
 	double* mesh;
+
 	//ReadInputMesh returning 1D array containing values
 	mesh = readInputMesh(filename, &nTriangles, &nMeshEntries);
 
@@ -30,10 +26,8 @@ int main() {
 		printf("mesh[%d]=%f\n", i, mesh[i]);
 	}*/
 
-	int i, j;
-
 //Reading inputs from "readInputSpheres.c" 
-//	int i,j;
+	int i,j;
 	int n_cols = 3;
 	double rad;
 	int ns;
@@ -44,6 +38,7 @@ int main() {
 	printf("Start of triangle TESTING\n");
 	printf("nt = %d\n", nTriangles);
 	printf("entries = %d\n", nMeshEntries);
+
 	//prints x1 y1 z1 x2 y2 z2 x3 y3 z3 coordinates for each triangle
   	for (i = 0; i < nTriangles; i++) {
     		for (j = 0; j < 9; j++) {
@@ -53,9 +48,10 @@ int main() {
   	}
 	printf("End of triangle TESTING\n");
 
+	//prints x y z coordinates for each sphere center
 	printf("\nStart of sphere TESTING\n");
 	printf("radius of spheres --> %f\n", rad);
-	printf("Number of spheres --> %f\n", ns);
+	printf("Number of spheres --> %d\n", ns);
   	for (i = 0; i < ns; i++) {
     		for (j = 0; j < n_cols; j++) {
       		printf("%lf ", sphereArr[i * n_cols + j]);
@@ -64,17 +60,19 @@ int main() {
   	}
 	printf("End of sphere TESTING\n");
 
-// Collision detection, order: (s,t) ; (i,j) ; (sphere,triangle)
-// Need to compute all (point - point), (point - line), (point - plane)
+// Collision detection
+// Need to compute all : 1st) - (point - plane)  ;  2nd) - (point - line)  ;  3rd) - (point - point), 
 
 	// Computes point - point (distance from sphere center to each vertex of triangle)
 	// d = sqrt((x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2)
 	int spheres, tri;
 	double d1,d2,d3;
-	printf("\nDistance to vertices\n");
+	double ax, ay, az, bx, by, bz, nxx, nyy, nzz, nx, ny, nz;
+	double dpx, dpy, dpz, dist, pox, poy, poz;
 	for (spheres = 0; spheres < ns; spheres++) {
 		for (tri = 0; tri < nTriangles; tri++) {
-			printf("Sphere ID: %d  ---  Triangle ID: %d\n", spheres, tri);
+			printf("\nSphere ID: %d  ---  Triangle ID: %d\n", spheres, tri);
+			printf("Distance to vertices\n");
 			d1 = sqrt(pow((sphereArr[spheres * n_cols + 0]-mesh[tri * 9 + 0]),2)+pow((sphereArr[spheres * n_cols + 1]-mesh[tri * 9 + 1]),2)+pow((sphereArr[spheres * n_cols + 2]-mesh[tri * 9 + 2]),2));
 			printf("d1 = %f   ", d1);
 
@@ -84,21 +82,15 @@ int main() {
 			d3 = sqrt(pow((sphereArr[spheres * n_cols + 0]-mesh[tri * 9 + 6]),2)+pow((sphereArr[spheres * n_cols + 1]-mesh[tri * 9 + 7]),2)+pow((sphereArr[spheres * n_cols + 2]-mesh[tri * 9 + 8]),2));
 			printf("d3 = %f\n", d3);
 			//will put into array
-		}
-	}
+
 
 // point - line
+// 1st) point - plane -- then detect if point inside triangle on plane
 
-// 1st) point - plane -- then if point inside triangle on plane -- point could be on plane outside of triangle
-
-	//calculateing normal vector to triangle (p1, p2, p3)
-	// a = p2-p1  ;  b = p3-p1
-	// nx = ay*bz - az*by  ;  ny = az*bx - ax*bz  ;  nz = ax*by - ay*bx
-	double ax, ay, az, bx, by, bz, nxx, nyy, nzz, nx, ny, nz;
-	printf("\nNormal Vectors for Triangles\n");
-//	for (spheres = 0; spheres < ns; spheres++) {
-		for (tri = 0; tri < nTriangles; tri++) {
-			printf("Triangle ID: %d\n", tri);
+			//calculateing normal vector to triangle (p1, p2, p3)
+			// a = p2-p1  ;  b = p3-p1
+			// nx = ay*bz - az*by  ;  ny = az*bx - ax*bz  ;  nz = ax*by - ay*bx
+			printf("Normal Unit Vectors for Triangles\n");
 			ax = mesh[tri * 9 + 3] - mesh[tri * 9 + 0];
 			ay = mesh[tri * 9 + 4] - mesh[tri * 9 + 1];
 			az = mesh[tri * 9 + 5] - mesh[tri * 9 + 2];
@@ -114,8 +106,30 @@ int main() {
 			ny = nyy/sqrt(pow(nxx,2)+pow(nyy,2)+pow(nzz,2));
 			nz = nzz/sqrt(pow(nxx,2)+pow(nyy,2)+pow(nzz,2));
 			printf("nx = %f  :  ny = %f  :  nz = %f\n", nx, ny, nz);
+
+//			D = -(nxx*mesh[tri * 9 + 0])-(nyy*mesh[tri * 9 + 1])-(nzz*mesh[tri * 9 + 2]);
+
+			//distance from P to point in plane(on triangle)
+			dpx = ((sphereArr[spheres * n_cols + 0]-mesh[tri * 9 + 0])*nx);
+			dpy = ((sphereArr[spheres * n_cols + 1]-mesh[tri * 9 + 1])*ny);
+			dpz = ((sphereArr[spheres * n_cols + 2]-mesh[tri * 9 + 2])*nz);
+//			printf("dpx = %f  :  dpy = %f  :  dpz = %f\n", dpx, dpy, dpz);
+
+
+			// UNSURE OF ACCURACY OF PROJECTED POINT AND CLOSEST POINT
+
+			//Projected point from P to closest point in plane P0
+			pox = (sphereArr[spheres * n_cols + 0]-(dpx*nx));
+			poy = (sphereArr[spheres * n_cols + 1]-(dpy*ny));
+			poz = (sphereArr[spheres * n_cols + 2]-(dpz*nz));
+			printf("New closest point on plane : (pox = %f , poy = %f , poz = %f) \n", pox, poy, poz);
+
+			// closest distance
+			dist = sqrt(pow((sphereArr[spheres * n_cols + 0]-pox),2)+pow((sphereArr[spheres * n_cols + 1]-poy),2)+pow((sphereArr[spheres * n_cols + 2]-poz),2));
+			printf("Distance to closest point on plane = %f \n",dist);			
+			
 		}
-//	}
+	}
 
 
 	//free values
