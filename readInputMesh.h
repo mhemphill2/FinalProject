328e4c007079711acdb56de236ccc58a9cc093d0
nsholm_win <56319291+nikolaj-h@users.noreply.github.com>
2019-12-11 17:14:01 -0600

    added doxygen comments on .h files

--- readInputMesh.h
+++ readInputMesh.h
@@ -1,5 +1,29 @@
 // Author: Nikolaj Holm
 
+
+/**
+* \details The function reads data from a file, containing a mesh built of 
+* triangles, store it on the local machine and output a 1D array containing
+* the data. The function alters the parameters **nTriangles** and 
+* **nMeshEntries** during execution, based on the data. The filename must be
+* given.
+* 
+* \param filname Pointer to an array of type char, that contains the filename.
+* \param nTriangles Contains the number of triangles in the mesh.
+* \param nMeshEntries Contains the number of total coordinates in the mesh.
+*
+* \return Entries Returns an array containing vertities
+*
+* Given a filename the function reads in a mesh consisting of triangles. 
+* The file must be formatted as follows:
+*
+* - The first line contain the number of triangles.
+* - All following lines come in sets of three. Each set of three lines 
+*	represent one triangel.
+* - Each of theese three lines contain a x,y and z coordinate representing each node in the triangle.
+*
+*/
+
 #ifndef readInputMesh_H
 #define readInputMesh_H
 
--- readInputSpheres.h
+++ readInputSpheres.h
@@ -1,5 +1,29 @@
 // Author: Michael Hemphill
 
+/**
+* \details The function reads data from a file with a given filename. 
+* The data is stored and return a 1D array containing the sphere
+* coordinates. The function alters the parameters **radius** and **nSpheres** 
+* based on the data.
+*
+* \param filname1 Pointer to an array of type char, that contains the filename.
+* \param radius Contains the number of triangles in the mesh.
+* \param nSpheres Contains the number of total coordinates in the mesh.
+*
+* \return Entries Returns an array containing sphere coordinates
+*
+* Given a filename the function reads in a list of sphere coordinates.
+* The file must be formatted as follows:
+*
+* - The first line contains a header "x,y,z".
+* - The next line contain the sphere radius.
+* - The third line contain the number of spheres.
+* - All following lines contain a x,y and z coordinate representing the center
+* of each sphere.
+*
+*
+*/
+
 #ifndef readInputSpheres_H
 #define readInputSpheres_H
 
